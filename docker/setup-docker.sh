c8aebb6ae1ecd320f91918236846ed9cb3f2a076
Ardavon Falls <afalls@qvxlabs.com>
2024-12-29 00:13:29 -0800

    Initial commit of docker build image

---
+++ docker/setup-docker.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# create docker buildx builder
+docker buildx create --name gameshellsdk
+docker buildx inspect gameshellsdk --bootstrap
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+
+# build docker images
+docker buildx build --platform linux/arm/v7 \
+       --builder gameshellsdk \
+       --output type=docker \
+       -t mobilegamemaker/gameshellsdk/clockworkos:0.5 \
+       ClockworkOS/0.5/
+
