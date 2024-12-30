7109b61da05b72b3a66937edd6a666cc9ca8ab7b
Ardy123 <afalls@qvxlabs.com>
2024-12-30 00:56:27 -0800

    Added Hello World Example (#1)
    * Added helloworld example
    * Added support for compiling static libraries
    * Created cmake toolchain file

---
+++ cmake/gcc-clockworkos.cmake
@@ -0,0 +1,22 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_VERSION 1)
+set(CMAKE_SYSTEM_PROCESSOR armv7l)
+
+get_filename_component(
+  GAMESHELL_SDK_ROOT "${CMAKE_CURRENT_LIST_DIR}" DIRECTORY)
+  
+set(CMAKE_C_COMPILER "${GAMESHELL_SDK_ROOT}/bin/gcc-clockworkos-0.5")
+set(CMAKE_CXX_COMPILER "${GAMESHELL_SDK_ROOT}/bin/g++-clockworkos-0.5")
+set(CMAKE_ASM_COMPILER "${GAMESHELL_SDK_ROOT}/bin/as-clockworkos-0.5")
+set(CMAKE_AR "${GAMESHELL_SDK_ROOT}/bin/ar-clockworkos-0.5")
+set(CMAKE_RANLIB "${GAMESHELL_SDK_ROOT}/bin/ranlib-clockworkos-0.5")
+
+set(DEFAULT_FLAGS "-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard")
+if(CLOCKWORK_ENABLE_THUMB)
+  set(DEFAULT_FLAGS "${DEFAULT_FLAGS} -mthumb")
+else()
+  set(DEFAULT_FLAGS "${DEFAULT_FLAGS} -marm")
+endif()
+  
+set(CMAKE_C_FLAGS "${DEFAULT_FLAGS}")
+set(CMAKE_CPP_FLAGS "${DEFAULT_FLAGS}")
---
+++ examples/helloworld/helloworld.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[]) {
+  // exclude unused params
+  (void)argc;
+  (void)argv;
+  // print message
+  printf("Hello World\n");
+  return 0;
+}
